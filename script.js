3d72d2ccba3a7bc84ac974e34b76ae1829029690
AnonimDinosaur <oriolsalasobrino@gmail.com>
2024-06-05 20:45:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ document.getElementById("Buyn").addEventListener("click", function() {
         nouApartat.style.display = "none";
     }
 });
-
-
+/* Extellio - acmod */
 (function(d,h,s){
-s=d.createElement('script');
-s.async=1;s.src=h;
-d.getElementsByTagName('head')[0].appendChild(s);
-})(document,'https://script.extellio.com/heartfelt-wisp-1cce1f-netlify-app.min.js');
+    s=d.createElement('script');
+    s.async=1;s.src=h;
+    d.getElementsByTagName('head')[0].appendChild(s);
+    })(document,'https://script.extellio.com/heartfelt-wisp-1cce1f-netlify-app.min.js');
+
